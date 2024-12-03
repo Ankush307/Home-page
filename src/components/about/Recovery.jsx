29aa588e84d39b0893d1e66340eb6fed67c934de
Ankush307 <ankushdhull966@gmail.com>
2024-12-03 14:22:11 +0530

    edit code

--- src/components/about/Recovery.jsx
+++ src/components/about/Recovery.jsx
@@ -2,15 +2,28 @@ import { useState } from 'react';
 import React from 'react'
 
 const Recovery = () => {
-    const [value, setValue] = useState(1);
-    const handleClick = () => {
-        setValue(value - 1);
+    const [value, setValue] = useState(0);
+
+
+    const handClicker = () => {
+        setValue(value + 1);
     };
+    const handDeclicker = () => {
+        setValue(value - 1);
+    }
     return (
-        <div>
-            <h1 className="md:text-5xl font-bold md:mb-10 my-3 text-3xl">Use State</h1>
-            <p className="text-xl font-bold mb-5"> {value}</p>
-            <button onClick={handleClick} className='bg-black text-white py-2 px-5 rounded-lg'>Click Me</button>
+        <div className="bg-white pb-40">
+            <h2 className='text-black mb-5 text-center text-[72px] godzillnpma'>Counting</h2>
+            <div className="flex border-[5px] max-w-[400px] h-[141px] rounded-[50px] bg-[#1EABF8] border-black  items-center justify-center mx-auto">
+                <button
+                    onClick={handDeclicker}
+                    className=" text-black px-16 font-normal leading-[24px] text-[60px]  py-4 rounded mx-auto block"> -
+                </button>
+
+                <p className="text-black text-center  leding-[24px] text-[60px]"> {value}</p>
+                <button onClick={handClicker}
+                    className=" text-black px-16 font-normal text-[60px] leading-[24px]  py-4 rounded mx-auto block">+</button>
+            </div>
         </div>
     )
 }
