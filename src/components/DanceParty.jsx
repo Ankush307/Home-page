049bf9b7e3b514fb6a6d9a2f22a973b4603f7d48
Ankush307 <ankushdhull966@gmail.com>
2025-02-08 15:16:34 +0530

    changes

--- src/components/DanceParty.jsx
+++ src/components/DanceParty.jsx
@@ -9,15 +9,15 @@ const DanceParty = () => {
 
     return (
         <div className='bg-black min-h-screen flex items-center justify-center flex-col'>
-            <button onClick={toggleDance} className={`absolute top-10 bg-white py-2 px-6 rounded-lg font-bold text-lg shadow-lg transform transition-all duration-300 ease-in-out ${isActive ? 'bg-lime-600 text-white' : ''} `}>Change Dance Pose </button>
-            <h1 className="text-white font-extrabold text-6xl mb-12"> DANCE PARTY!</h1>
+            <button onClick={toggleDance} className={`absolute top-10 bg-white py-2 px-6 rounded-lg font-bold text-lg shadow-lg transform transition-all duration-300 ease-in-out ${isActive ? '!bg-slate-600 text-white' : ''} `}>Change Dance Pose </button>
+            <h1 className="text-white font-extrabold md:text-6xl sm:text-5xl text-4xl mb-12"> DANCE PARTY!</h1>
             <div className="w-full flex items-center justify-center flex-col">
                 {/* Head */}
                 <div className={`rounded-2xl shadow-inner w-[80px] h-[80px] bg-white ${isActive ? 'dancer-head' : 'head'}`} />
                 {/* Body */}
                 <div className="rounded-2xl shadow-inner w-[20px] body h-[120px] mt-3 bg-white" />
                 {/* Arms */}
-                <div className="flex gap-20 w-[500px] items-center justify-center translate-y-[-100px] relative z-10">
+                <div className="flex gap-20 sm:w-[500px] w-full items-center justify-center translate-y-[-100px] relative z-10">
                     <div className={`rounded-2xl w-[100px] shadow-inner h-[20px] bg-white ${isActive ? 'dancer-arm origin-right' : 'left-arm'}`} />
                     <div className={`rounded-2xl w-[100px] shadow-inner h-[20px] bg-white ${isActive ? 'dancer-arm origin-left' : 'right-arm'}`} />
                 </div>
